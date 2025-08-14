f5c5c9549c26a5d9e2b94a11f1cc0eb372175ff4
Yevhen Y <evgeniy.yakovlev@mserverone.com>
2025-08-14 11:26:35 +0200

    Update menu

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
     Telegram.WebApp.ready();
+    const userId = Telegram.WebApp.initDataUnsafe.user.id;
+    const chatId = Telegram.WebApp.initDataUnsafe.chat.id;
 
     document.querySelectorAll('.tg-command').forEach(button => {
         button.addEventListener('click', function (event) {
             event.preventDefault();
             Telegram.WebApp.sendData(this.dataset.command);
+        })
+    })
+
+    document.querySelectorAll('.chat-link').forEach(button => {
+        button.addEventListener('click', function (event) {
+            event.preventDefault();
+            let url = this.dataset.url + '&user=' + userId + '&chat=' + chatId;
+            window.open(url, '_blank');
         })
     })
 });
